04f0a4f4bac0e42c18b46eea0b0c5a193f0352a0
Romeu Gomes <oh4ianon@hotmail.com>
2016-10-17 18:15:18 +0100

    Decryption working.

--- src/main.c
+++ src/main.c
@@ -31,17 +31,31 @@ void encrypt(char *str, int shift, size_t len)
 	}
 }
 
-void decrypt(char *str, int shift)
+void decrypt(char *str, int shift, size_t len)
 {
+	uint8_t i;
 
+	for(i = 0; i < len; i++)
+	{
+		if((str[i] - shift) >= 65 && (str[i] - shift) <= 90)
+		{
+			str[i] -= shift;
+		}
+		else
+		{
+			str[i] -= (shift - ('Z' - 'A'));
+		}
+	}
 }
 
 int main()
 {
 	char test[] = "Teste";
 
-	encrypt(test, 12, strlen(test));
+	encrypt(test, 24, strlen(test));
+	printf("%s\n", test);
 
+	decrypt(test, 24, strlen(test));
 	printf("%s\n", test);
 
 	return 0;
