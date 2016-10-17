372ef1ad96df4982ad3a2333b8824d334fdabcbb
Romeu Gomes <oh4ianon@hotmail.com>
2016-10-17 16:45:58 +0100

    Enable debugging. Encrypt function not working.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,17 @@
 
 void encrypt(char *str, int shift)
 {
-
+	while(*str++ != '\0')
+	{
+		if((*str + shift) >= 65 && (*str + shift) <= 90)
+		{
+			*str += shift;
+		}
+		else
+		{
+			*str += (shift - ('Z' - 'A'));
+		}
+	}
 }
 
 void decrypt(char *str, int shift)
@@ -20,7 +30,11 @@ void cs_toupper(char *str)
 
 int main()
 {
-	
+	char test[] = "Teste\0";
+
+	encrypt(test, 1);
+
+	printf("%s\n", test);
 
 	return 0;
 }
