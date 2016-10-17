92b7179fa01ae3a69f27aae65aca9bb11007e5a4
Romeu Gomes <oh4ianon@hotmail.com>
2016-10-17 16:25:01 +0100

    To Uppercase function.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+void encrypt()
+{
+
+}
+
+void decrypt()
+{
+
+}
+
+void cs_toupper(char *str)
+{
+	while(*str++ != '\0')
+	{
+		*str = (*str & '_');
+	}
+}
+
 int main()
 {
+	char test[] = "Test";
+
+	cs_toupper(test);
+
+	printf("%s\n", test);
+
 	return 0;
 }
